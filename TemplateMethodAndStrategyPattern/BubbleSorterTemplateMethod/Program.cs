7b8c15405d355aec728f6b589af1b13b6459c069
Bill Ho <ycho.bill.lab@gmail.com>
2022-11-27 17:36:47 +0800

    [BubbleSorter][TemplateMethod] init by copy project

---
+++ TemplateMethodAndStrategyPattern/BubbleSorterTemplateMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World, Sorter!");
+
+int[] random_array = new int[] {5, 2, 9, 1};
+int operations = BubbleSorter.Sort(random_array);
+for (int i = 0; i < random_array.Length; i++) {
+    Console.WriteLine($"{i} {random_array[i]}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"operations: {operations}");
