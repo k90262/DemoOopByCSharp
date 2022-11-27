e0457df7354c84392807916960b780245fcbf007
Bill Ho <ycho.bill.lab@gmail.com>
2022-11-27 17:46:55 +0800

    [BubbleSorter][TemplateMethod] added double-type child class by using existed parent class with the same business logic

--- TemplateMethodAndStrategyPattern/BubbleSorterTemplateMethod/Program.cs
+++ TemplateMethodAndStrategyPattern/BubbleSorterTemplateMethod/Program.cs
@@ -10,3 +10,14 @@ for (int i = 0; i < random_array.Length; i++) {
 
 Console.WriteLine();
 Console.WriteLine($"operations: {operations}");
+
+
+double[] random_array_double = new double[] {5.2, 2, 9.3, 1};
+DoubleBubbleSorter sorter_double = new DoubleBubbleSorter();
+int operations_double = sorter_double.Sort(random_array_double);
+for (int i = 0; i < random_array_double.Length; i++) {
+    Console.WriteLine($"{i} {random_array_double[i]}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"operations: {operations_double}");
