7e7f5333a79ed3d03cae1f91fc77273e3a783dd0
Bill Ho <ycho.bill.lab@gmail.com>
2023-01-07 23:11:50 +0800

    [BubbleSorter][Strategy] init by coping template-method project

---
+++ TemplateMethodAndStrategyPattern/BubbleSorterStrategy/BubbleSorter.cs
@@ -0,0 +1,26 @@
+public abstract class BubbleSorter 
+{
+    private int operations = 0;
+    protected int length = 0;
+
+    protected int DoSort() 
+    {
+        operations = 0;
+        if (length <= 1)
+            return operations;
+        
+        for (int nextToLast = length-2; nextToLast >= 0; nextToLast--)
+            for (int index = 0; index <= nextToLast; index++)
+            {
+                if (OutOfOrder(index))
+                    Swap(index);
+                operations++;
+            }
+        
+        return operations;
+    }
+
+    protected abstract bool OutOfOrder(int index);
+
+    protected abstract void Swap(int index);
+}
---
+++ TemplateMethodAndStrategyPattern/BubbleSorterStrategy/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World, Sorter!");
+
+int[] random_array = new int[] {5, 2, 9, 1};
+IntBubbleSorter sorter = new IntBubbleSorter();
+int operations = sorter.Sort(random_array);
+for (int i = 0; i < random_array.Length; i++) {
+    Console.WriteLine($"{i} {random_array[i]}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"operations: {operations}");
+
+
+double[] random_array_double = new double[] {5.2, 2, 9.3, 1};
+DoubleBubbleSorter sorter_double = new DoubleBubbleSorter();
+int operations_double = sorter_double.Sort(random_array_double);
+for (int i = 0; i < random_array_double.Length; i++) {
+    Console.WriteLine($"{i} {random_array_double[i]}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"operations: {operations_double}");
